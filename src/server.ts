967ec2a458ea8d5329452c04ed7c8e883a83f61e
DwYoo <aydw@123gmail.com>
2023-12-14 03:28:41 +0900

    .

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
-import router from "./routes/index";
+import express from 'express';
+import router from './routes/index';
 
-var express = require('express');
-var app = express();
+// Express 애플리케이션 생성
+const app = express();
+
+// 라우터를 사용하여 경로를 설정
 app.use('/', router);
+
+// 서버 시작
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
